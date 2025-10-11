1e79ead4cb6cfd1e51eaeafe4929a79e32e1f749
Dike Kingsley <125186879+Pure-coder007@users.noreply.github.com>
2025-10-11 15:36:59 +0100

    Load environment variables from .env file
    Added python-dotenv to load environment variables from a .env file in the project root. Improved SECRET_KEY and ALLOWED_HOSTS handling to raise an error if SECRET_KEY is missing and to parse ALLOWED_HOSTS from environment variables.

--- educore/settings.py
+++ educore/settings.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 from django.conf.global_settings import AUTH_USER_MODEL, SECRET_KEY
 import os
 from rest_framework.permissions import IsAuthenticated
@@ -7,14 +8,18 @@ from rest_framework.permissions import IsAuthenticated
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY is missing from environment variables.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
 
 
 # Application definition
